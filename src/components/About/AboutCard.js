45902d498c20dd1a2347ffc3e03103fea87cb8d3
Prasoon Bharat Mishra <bharatprasoon@gmail.com>
2024-12-10 23:47:03 +0530

    first

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+          Hi Everyone, I am <span className="purple">Anuj Sharma </span>
+            from <span className="purple"> College Park, Maryland.</span>
+            <br />
+            I am currently pursuing a Master of Science in Information Systems at the University of Maryland and have a strong background in data analytics and business intelligence.
+            <br />
+           
+            <br />
+            <br />
+            Apart from crunching data, some other activities that I love to do:
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Exploring innovative tech solutions
+            </li>
+            <li className="about-activity">
+              <ImPointRight />Mentoring peers in analytics and visualization
+            </li>
+            <li className="about-activity">
+              <ImPointRight />Keeping fit with sports and fitness activities
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+          "Strive to turn data into actionable insights!"{" "}
+          </p>
+          <footer className="blockquote-footer">Anuj</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+            Hi, I’m Anuj Sharma, a data and machine learning professional passionate about turning raw data into impactful insights. 
+              <br />
+              <br />With expertise in
+              <i>
+                <b className="purple"> Python, SQL, Power BI,and machine learning. </b>
+              </i>
+              <br />
+              <br />
+              I deliver solutions that drive decision-making and optimize processes.
+
+              From predictive modeling to designing dashboards that tell compelling stories, I excel at crafting data-driven strategies to solve real-world challenges. Explore my portfolio to see how I blend advanced analytics with business intelligence to create measurable impact.
+
+              <i>
+                
+               
+              </i>
+              <br />
+              <br />
+              <b className="purple">Let’s unlock the potential of data together!</b>
+              
+                
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/soumyajit4419"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              {/* <li className="social-icons">
+                <a
+                  href="https://twitter.com/Soumyajit4419"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li> */}
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/sharmaanuj10/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              {/* <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/soumyajit4419"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li> */}
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
