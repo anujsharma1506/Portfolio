ec8fc925e71414dbdfa4e4cd8ded48751d346971
Prasoon Bharat Mishra <bharatprasoon@gmail.com>
2024-12-16 21:35:48 +0530

    changes

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -13,6 +13,18 @@ import {
   
 } from "react-icons/di";
 import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiTableau,
+  SiSnowflake,
+  SiAmazonaws,
+  SiAzuredevops,
+  SiVercel,
+  SiMicrosoftexcel,
+  SiMacos,
+} from "react-icons/si";
+import {
   SiRedis,
   SiFirebase,
   SiNextdotjs,
@@ -27,27 +39,27 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      {/* <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
-      </Col>
+      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiMysql />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      {/* <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      </Col> */}
+      {/* <Col xs={4} md={2} className="tech-icons">
         <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      </Col> */}
+      {/* <Col xs={4} md={2} className="tech-icons"> */}
         {/* <SiSolidity />
       </Col>
       <Col xs={4} md={2} className="tech-icons"> */}
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
+        {/* <DiMongodb />
+      </Col> */}
+      {/* <Col xs={4} md={2} className="tech-icons">
         <SiNextdotjs />
-      </Col>
+      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
@@ -60,14 +72,46 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      {/* <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+      </Col> */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMacos />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAmazonaws />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTableau />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftexcel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAzuredevops />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSnowflake />
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSlack />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
       </Col>
       {/* <Col xs={4} md={2} className="tech-icons">
         <DiJava />
       </Col> */}
     </Row>
   );
+
 }
 
 export default Techstack;
